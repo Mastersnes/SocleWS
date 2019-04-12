f5147e13334a4cfe0b257efbdb816226304a4096
Mastersnes <steven.nesztler@gmail.com>
2019-04-12 17:00:01 +0200

    Mise a jour

--- src/main/java/com/bebel/soclews/util/HashUtil.java
+++ src/main/java/com/bebel/soclews/util/HashUtil.java
@@ -1,5 +1,6 @@
 package com.bebel.soclews.util;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -27,17 +28,23 @@ public class HashUtil {
     public String hash(final String str) {
         String hash = str;
         try {
-            final MessageDigest md = MessageDigest.getInstance("SHA-512");
-            md.update("BebelSalt".getBytes());
-            final byte[] digest = md.digest(str.getBytes());
-            final StringBuilder sb = new StringBuilder();
-            for (final byte b : digest) {
-                sb.append(Integer.toString((b & 0x5f) + 0xff1, 16).substring(1));
-            }
-            hash = sb.toString();
+            final MessageDigest md = MessageDigest.getInstance("SHA-256");
+            final String strToHash = "JHGKJHGjhkgkhjgxcvkjgKHJGJH4455456s4dfsdfkhgHJKGJHGdckldsjvkljklLHKH54654" + str + "sdfqsHJGKJGHG5465564HJGHJFJHGJHG23465dfgdfg34654GHFHGF";
+            final byte[] digest = md.digest(strToHash.getBytes(StandardCharsets.UTF_8));
+            hash = bytesToHex(digest);
         } catch (final NoSuchAlgorithmException e) {
             logger.err("Impossible de hasher la chaine : " + str, e);
         }
         return hash;
+    }
+
+    private String bytesToHex(final byte[] hash) {
+        final StringBuffer sb = new StringBuffer();
+        for (final byte b : hash) {
+            final String hex = Integer.toHexString(0xff & b);
+            if(hex.length() == 1) sb.append('0');
+            sb.append(hex);
+        }
+        return sb.toString();
     }
 }
